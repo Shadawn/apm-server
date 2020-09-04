be3b75fe26f047380ecb1fb7407f5e2438d8bc38
Shadawn <esteraza@yandex.ru>
2020-09-04 14:00:36 +0300

    Heroku deployment

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const path = require('path');
 
 const app = express()
-const port = 6584
+const port = process.env.PORT || 3000;
 
 const packageListString = fs.readFileSync(path.join(__dirname, 'package-list.json'), 'utf-8');
 
