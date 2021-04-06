f4e3663f74543f726ae4c2433139095267a65e86
Shadawn <esteraza@yandex.ru>
2021-04-06 23:03:09 +0300

    Добавлена ссылка на клиент для 1С.

--- index.js
+++ index.js
@@ -8,11 +8,10 @@ const port = process.env.PORT || 3000;
 const packageListString = fs.readFileSync(path.join(__dirname, 'package-list.json'), 'utf-8');
 
 app.get('/packages', (req, res) => {
-  //console.log(typeof packageListString)  
   res.type('json')
   res.send(packageListString)
-  //res.send('Hello world!');
 })
+app.use(express.static('static'))
 
 app.listen(port, () => {
   console.log(`Package manager listening at http://localhost:${port}`)
